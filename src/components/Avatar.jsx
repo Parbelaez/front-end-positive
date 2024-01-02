d602381a05f9f9fc7f56aa91200fa8e77478f5f3
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-02 16:17:32 +0000

    Create the Avatar module

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,17 @@
+import styles from "../styles/Avatar.module.css";
+
+const Avatar = ({ src, height = 45 }) => {
+    return (
+        <span>
+            <img
+                className={styles.Avatar}
+                src={src}
+                height={height}
+                width={height}
+                alt="avatar"
+            />
+        </span>
+    );
+};
+
+export default Avatar;
