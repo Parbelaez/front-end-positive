824b129b7686196e3c746a3bd2c5965daec4ca0e
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-02 20:27:24 +0000

    Add the Current User provider to the index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouteras as Router } from 'react-router-dom';
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
     </Router>
   </React.StrictMode>
 );
