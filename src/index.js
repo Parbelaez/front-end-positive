765d4a0170426e60f6f19a3a3997518999c804ff
Paulo Arbeláez <pauloarbelaez@gmail.com>
2024-01-03 14:03:41 +0000

    ROLL-BACK: ReactDOM.render is deprecated

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
-
-ReactDOM.render(
-  <Router>
-    <CurrentUserProvider>
-      {/* <ProfileDataProvider> */}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <CurrentUserProvider>
         <App />
-      {/* </ProfileDataProvider> */}
-    </CurrentUserProvider>
-  </Router>,
-  document.getElementById("root")
+      </CurrentUserProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
